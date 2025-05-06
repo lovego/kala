bd95b1f6983e6fe552a374808ebf259f3a5aade2
廖恒晨 <liaohengchen@gmail.com>
2025-05-06 16:24:10 +0800

    client

--- client/schedule.go
+++ client/schedule.go
@@ -2,15 +2,17 @@ package client
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 
+	"github.com/lovego/kala/types"
 	"github.com/lovego/kala/utils/iso8601"
 )
 
 type Schedule struct {
 	After    time.Duration
 	Interval iso8601.Duration
-	Repeat   int
+	Repeat   uint
 }
 
 // Make Schedule for create job.
@@ -23,4 +25,34 @@ func (s Schedule) String() string {
 		schedule += "/PT0S"
 	}
 	return schedule
+}
+
+type Scheduler struct {
+	Name      string                 // job name
+	Owner     string                 // job owner
+	GroupName string                 // job group name
+	Content   string                 // job content
+	Remote    types.RemoteProperties // job remote properties
+	Retries   uint                   // job retry times when failed
+	Schedule
+}
+
+func (s Scheduler) Job() *types.Job {
+	job := &types.Job{
+		Name:             s.Name,
+		Owner:            s.Owner,
+		GroupName:        s.GroupName,
+		Content:          s.Content,
+		Schedule:         s.Schedule.String(),
+		Retries:          s.Retries,
+		JobType:          types.RemoteJob,
+		RemoteProperties: s.Remote,
+	}
+	if job.RemoteProperties.Headers == nil {
+		job.RemoteProperties.Headers = make(http.Header)
+	}
+	if job.Retries == 0 {
+		job.Retries = 1 // default retry 1 times
+	}
+	return job
 }
